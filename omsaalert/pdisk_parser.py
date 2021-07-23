eeaae01b9b106f1b78c49bbde6eb5fd359dd3c06
chrisyunhua <chrisyunhua@gmail.com>
2021-07-23 13:07:56 +0800

    Support add acceptable pdisk status

--- omsaalert/pdisk_parser.py
+++ omsaalert/pdisk_parser.py
@@ -5,14 +5,14 @@ import omsaalert.utility
 
 _LOGGER = logging.getLogger(__name__)
 
-_NONGLOBALHOTSPARE_HEALTHY_VALUES = {
-    'Status': ['Ok'],
-    'State': ['Online'],
+NONGLOBALHOTSPARE_HEALTHY_VALUES = {
+    "Status": ["Ok"],
+    "State": ["Online"],
 }
 
-_GLOBALHOTSPARE_HEALTHY_VALUES = {
-    'Status': ['Ok'],
-    'State': ['Ready'],
+GLOBALHOTSPARE_HEALTHY_VALUES = {
+    "Status": ["Ok"],
+    "State": ["Ready"],
 }
 
 
@@ -21,24 +21,24 @@ class PdiskParser(omsaalert.parser.ParserBase):
         blocks = self.ssv_gen(content)
         blocks = list(blocks)
 
-        assert \
-            len(blocks) == 1, \
-            "Expected exactly one block of physical-disk data in the output:\n{}".format(blocks)
+        assert (
+            len(blocks) == 1
+        ), "Expected exactly one block of physical-disk data in the output:\n{}".format(
+            blocks
+        )
 
         disks = blocks[0]
 
         problems = []
         for disk in disks:
-            if disk['Hot Spare'] == 'Global':
-                error_message = \
-                    omsaalert.utility.check_expected_values(
-                        disk,
-                        _GLOBALHOTSPARE_HEALTHY_VALUES)
+            if disk["Hot Spare"] == "Global":
+                error_message = omsaalert.utility.check_expected_values(
+                    disk, GLOBALHOTSPARE_HEALTHY_VALUES
+                )
             else:
-                error_message = \
-                    omsaalert.utility.check_expected_values(
-                        disk,
-                        _NONGLOBALHOTSPARE_HEALTHY_VALUES)
+                error_message = omsaalert.utility.check_expected_values(
+                    disk, NONGLOBALHOTSPARE_HEALTHY_VALUES
+                )
 
             if error_message is not None:
                 problems.append((error_message, disk))
